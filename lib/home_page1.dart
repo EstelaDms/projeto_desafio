966f4c524847496d2cfa9972e153c233697a9934
EstelaDms <106538021+EstelaDms@users.noreply.github.com>
2022-05-30 15:03:34 -0300

    atualização

---
+++ lib/home_page1.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main(){
+
+  main() => runApp (ProjetoApp());
+
+
+@override
+Widget build(BuildContext context) {
+  return MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(const Color.fromARGB(255, 71, 100, 182),
+        title: Text const ("Home"),
+
+      ),
+    ),
+  ));
+}
+
+class ProjetoApp {
+}
+
+void body(Scaffold scaffold, Stack stack) {
+}
